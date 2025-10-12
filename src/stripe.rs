3852fd6fdf69e3336177d87192bb40e6c499db95
derdilla <contact@derdilla.com>
2025-10-12 11:18:38 +0200

    Implement Road

--- src/stripe.rs
+++ src/stripe.rs
@@ -1,4 +1,3 @@
-use std::fmt::{Display, Formatter};
 use crossterm::style::Stylize;
 use rand::distr::weighted::WeightedIndex;
 use rand::prelude::Distribution;
@@ -8,14 +7,16 @@ pub enum Stripe {
     Empty,
     Green(GreenStripe),
     Rail(Railroad),
+    Road(Road),
 }
 
 impl Stripe {
     pub fn generate() -> Self {
-        let dist = WeightedIndex::new([5, 3]).unwrap();
+        let dist = WeightedIndex::new([5, 3, 5]).unwrap();
         match dist.sample(&mut rand::rng()) {
             0 => Stripe::Green(GreenStripe::generate()),
             1 => Stripe::Rail(Railroad::generate()),
+            2 => Stripe::Road(Road::generate()),
             _ => panic!("Weighted index out of expected range"),
         }
 
@@ -26,6 +27,7 @@ impl Stripe {
             Stripe::Empty => {},
             Stripe::Green(stripe) => stripe.update(),
             Stripe::Rail(stripe) => stripe.update(),
+            Stripe::Road(stripe) => stripe.update(),
         }
     }
 
@@ -34,6 +36,7 @@ impl Stripe {
             Stripe::Empty => false,
             Stripe::Green(stripe) => stripe.collides(x),
             Stripe::Rail(stripe) => stripe.collides(x),
+            Stripe::Road(stripe) => stripe.collides(x),
         }
     }
 
@@ -42,6 +45,7 @@ impl Stripe {
             Stripe::Empty => vec![], // Uninitialized
             Stripe::Green(stripe) => stripe.visualize(),
             Stripe::Rail(stripe) => stripe.visualize(),
+            Stripe::Road(stripe) => stripe.visualize(),
         }
     }
 }
@@ -114,6 +118,78 @@ impl Railroad {
         }.to_vec()
     }
 }
+
+#[derive(Debug, Copy, Clone)]
+struct Road {
+    cars: [bool; 7],
+    direction: bool,
+    current_car_len: i32,
+    /// Cycles in 0..=2.
+    offset: usize,
+}
+
+impl Road {
+    fn generate() -> Self {
+        let mut road = Road {
+            cars: [false; 7],
+            current_car_len: 0,
+            offset: 0,
+            direction: rand::random(),
+        };
+        for x in 0..7 {
+            road.advance_road();
+        }
+
+        road
+    }
+
+    fn update(&mut self) {
+        self.offset += 1;
+        self.offset %= 3;
+        if self.offset == 0 {
+            self.advance_road();
+        }
+    }
+
+    fn advance_road(&mut self) {
+        let new_tile = match self.current_car_len {
+            -1..=0 => false, // 2 tiles space between cars
+            ..-1 => rand::random(),
+            1 => true,
+            2 => rand::random(),
+            3.. => false,
+        };
+        if new_tile {
+            self.current_car_len = (self.current_car_len + 1).max(1);
+        } else { // The car ended
+            if self.current_car_len > 1 {
+                self.current_car_len = 1;
+            }
+            self.current_car_len -= 1;
+        }
+
+        if self.direction {
+            self.cars.rotate_left(1);
+            self.cars[6] = new_tile;
+        } else {
+            self.cars.rotate_right(1);
+            self.cars[6] = new_tile;
+        }
+    }
+
+    fn collides(&self, x: u8) -> bool {
+        self.cars[x as usize]
+    }
+
+    fn visualize(&self) -> Vec<Block> {
+        let car = Block::Red;
+        let road = Block::Gray;
+        self.cars
+            .map(|t| if t { car } else { road })
+            .to_vec()
+    }
+}
+
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Block {
